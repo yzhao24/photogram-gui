6883de57f0dca2a64a95bc5c0e1ca4bdbd553ab7
yzhao24 <165528874+yzhao24@users.noreply.github.com>
2024-05-08 03:12:01 +0000

    read part done

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,25 @@
+class PhotosController < ApplicationController
+
+  def index
+    matching_photos = Photo.all
+    @list_of_photos = matching_photos.order({:created_at => :desc})
+
+    render({ :template => "photo_templates/index" })
+  end  
+
+
+  def show
+    url_id = params.fetch("path_id")
+
+    matching_photos = Photo.where({ :id => url_id})
+
+    @the_photo = matching_photos.at(0)
+
+    @all_comments = @the_photo.comments.all
+
+    render({ :template => "photo_templates/show" })
+  end
+
+
+
+end  
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 
   get("/users/:path_username", { :controller => "users", :action => "show"})
 
+
+
+  get("/photos", { :controller => "photos", :action => "index"})
+  get("/photos/:path_id", { :controller => "photos", :action => "show"})
 end
