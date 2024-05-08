d50044b8cfdf8f4d6805c20a242d77913b3f54a0
yzhao24 <165528874+yzhao24@users.noreply.github.com>
2024-05-08 02:26:06 +0000

    users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,21 @@ class UsersController < ApplicationController
     render({ :template => "user_templates/index" })
   end
   
+
+  def show
+
+    url_username = params.fetch("path_username")
+
+    matching_usernames = User.where({ :username => url_username })
+
+    @the_user = matching_usernames.at(0)
+
+    if @the_user == nil
+      redirect_to("/404")
+    else  
+    render({ :template => "user_templates/show" })
+    end
+  end
+
+
 end  
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 
   get("/users", { :controller => "users", :action => "index" })
+
+  get("/users/:path_username", { :controller => "users", :action => "show"})
+
 end
